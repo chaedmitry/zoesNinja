d25a4b9b52ba27d9b75fec8441cf5a5f95582c2c
Dmitry Tsay <chaedmitry@gmail.com>
2021-11-05 23:54:34 +0000

    font fixes, illustration placement

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,13 +4,13 @@ import Seo from "../components/seo"
 import girl from "../../static/zoe-new.webp"
 import { injectIntl, Link } from "gatsby-plugin-react-intl"
 
-const Home = ({ intl, pageContext }) => {
+const Home = ({ intl }) => {
   return (
       <Layout isHomePage>
         <Seo title={intl.formatMessage({id: "seo-home-title" })} />
-        <img className="author-illustration" alt="Illustration of Zoe" src={girl}/>
         <h1>{intl.formatMessage({id: "homepage-title" })}</h1>
         <p>{intl.formatMessage({id: "homepage-p1" })}</p>
+        <img className="author-illustration" alt="Illustration of Zoe" src={girl}/>
         <p>{intl.formatMessage({id: "homepage-p2" })}</p>
         <p>{intl.formatMessage({id: "homepage-p3" })}</p>
         <Link className="button button-accent start" to={`/chapters/01-the-first-encounter-${intl.locale}`}>{intl.formatMessage({id: "button-start" })}</Link>
