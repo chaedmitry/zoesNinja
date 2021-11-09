b9ee1e06b63491c198193488b104794a7f7e79d2
Dmitry Tsay <chaedmitry@gmail.com>
2021-11-09 21:58:02 +0000

    credits

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,6 +5,7 @@ const Footer = ({ intl }) => {
   return (
     <footer>
         <p>© {new Date().getFullYear()}{intl.formatMessage({id: "footer-title" })} <small>{intl.formatMessage({id: "footer-heart" })}</small></p>
+        <p class="secondary">Designed by <a class="secondary" href="https://chae.design/" target="_blank" rel="noreferrer">dmitrychae</a>, illustrated by&nbsp;<a class="secondary" href="https://www.instagram.com/o.tsay_/" target="_blank" rel="noreferrer">o.tsay</a></p>
     </footer>
   )
 }
