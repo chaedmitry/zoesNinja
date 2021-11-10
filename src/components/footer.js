a9d849737361474d4e448d3111d2914a1ad79b04
Dmitry Tsay <chaedmitry@gmail.com>
2021-11-10 22:59:51 +0000

    credits small, heart color, opt imgs, unic ranges

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,8 +4,8 @@ import { injectIntl } from "gatsby-plugin-react-intl"
 const Footer = ({ intl }) => {
   return (
     <footer>
-        <p>© {new Date().getFullYear()}{intl.formatMessage({id: "footer-title" })} <small>{intl.formatMessage({id: "footer-heart" })}</small></p>
-        <p class="secondary">Designed by <a class="secondary" href="https://chae.design/" target="_blank" rel="noreferrer">dmitrychae</a>, illustrated by&nbsp;<a class="secondary" href="https://www.instagram.com/o.tsay_/" target="_blank" rel="noreferrer">o.tsay</a></p>
+        <p>© {new Date().getFullYear()}{intl.formatMessage({id: "footer-title" })} <small class="heart">♥</small></p>
+        <p class="secondary"><small>Designed by <a class="secondary" href="https://chae.design/" target="_blank" rel="noreferrer">dmitrychae</a>, illustrated by&nbsp;<a class="secondary" href="https://www.instagram.com/o.tsay_/" target="_blank" rel="noreferrer">o.tsay</a></small></p>
     </footer>
   )
 }
