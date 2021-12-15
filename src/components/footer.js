74573792801854df21f4b512e86a553474283606
Dmitry Tsay <chaedmitry@gmail.com>
2021-12-15 23:28:14 +0600

    credits language sensitive

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,7 +5,7 @@ const Footer = ({ intl }) => {
   return (
     <footer>
         <p>© {new Date().getFullYear()}{intl.formatMessage({id: "footer-title" })} <small className="heart">♥</small></p>
-        <p className="secondary"><small>Designed by <a className="secondary" href="https://chae.design/" target="_blank" rel="noreferrer">dmitrychae</a>, illustrated by&nbsp;<a className="secondary" href="https://www.instagram.com/o.tsay_/" target="_blank" rel="noreferrer">o.tsay</a></small></p>
+        <div dangerouslySetInnerHTML={{__html: intl.formatMessage({id: "footer-credits" })}}></div>
     </footer>
   )
 }
