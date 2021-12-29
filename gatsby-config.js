43efb16197e310d90aa59109a6028e0094a0ab4f
Dmitry Tsay <chaedmitry@gmail.com>
2021-12-29 22:08:09 +0600

    add ga

--- gatsby-config.js
+++ gatsby-config.js
@@ -83,6 +83,21 @@ module.exports = {
         icon: `static/favicon.png`,
       },
     },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-EN3SM85SWR", // Google Analytics / GA
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+        },
+      },
+    },
   ],
   siteMetadata: {
     title: `Zoe's Ninja · 難言之隱`,
