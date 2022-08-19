ce2381fed76f9f99425edcd1ae78e194bff1624a
Dmitry Tsay <chaedmitry@gmail.com>
2022-08-19 09:56:51 +0100

    add gtag id

--- gatsby-config.js
+++ gatsby-config.js
@@ -89,6 +89,7 @@ module.exports = {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
         trackingIds: [
           "G-EN3SM85SWR", // Google Analytics / GA
+          "GTM-NMVRN52", // Google Tag Manager
         ],
         // This object gets passed directly to the gtag config command
         // This config will be shared across all trackingIds
